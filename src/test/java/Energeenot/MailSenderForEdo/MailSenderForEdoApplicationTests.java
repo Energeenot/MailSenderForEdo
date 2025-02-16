89c362209e401ced19278d6075a7335d649d79ea
Energeenot <abramov.pandos@yandex.ru>
2025-02-16 17:40:13 +0300

    Initial commit

---
+++ src/test/java/Energeenot/MailSenderForEdo/MailSenderForEdoApplicationTests.java
@@ -0,0 +1,13 @@
+package Energeenot.MailSenderForEdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderForEdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
